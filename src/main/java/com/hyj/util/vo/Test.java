1e5cd5ec6e4e0d9972823350a3c7ab869753a677
hyj <1046271658@qq.com>
2018-02-12 11:04:21 +0800

    Initial commit

---
+++ src/main/java/com/hyj/util/vo/Test.java
@@ -0,0 +1,12 @@
+package com.hyj.util.vo;
+
+/**
+ * Created by houyunjuan on 2018/2/12.
+ */
+public class Test {
+    public static void main(String[] args) {
+        for (int i = 0; i < 100 ; i++) {
+            System.out.println(i);
+        }
+    }
+}
